d952f327307f32cb3efa3f2ec1342d6aab439d95
Mike Roberts <mikebroberts@hotmail.com>
2019-10-15 21:54:10 -0600

    cleaning up sendEmail function

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,5 +1,6 @@
+//jshint esversion: 6
+
 const AWS = require("aws-sdk");
-const querystring = require("querystring");
 const dotenv = require('dotenv');
 const env = dotenv.config().parsed;
 
@@ -9,49 +10,25 @@ const ses = new AWS.SES({
   process.env["MY_AWS_SECRET_ACCESS_KEY"])
 });
 
-
-
-/** @typedef {function(Error=,Object=)} */
-var NetlifyCallback;
-
-/**
- * Returns just type and subtype from Content-Type HTTP header value.
- *
- * @param {string|undefined} headerValue
- * @return {string}
- */
+ //Returns just type and subtype from Content-Type HTTP header value.
 function parseContentType(headerValue) {
   return (headerValue || "").split(/;\s+/, 2)[0];
 }
 
-/**
- * Returns name encoded using syntax of encoded-words from MIME.
- *
- * This is a very lazy developer’s approach defaulting to BASE64
- * without trying anything else and shouldn’t be considered
- * production-ready. MIME suggests what to use when, get familiar with
- * or use some nice library.
- *
- * @param {string} name
- * @return {string}
- * @see {@link https://tools.ietf.org/html/rfc2047 RFC 2047}
- */
-function mimeEncode(name) {
-  return (
-    "=?utf-8?b?" +
-    Buffer.from(name).toString('base64') +
-    "?="
-  );
-}
+// // Returns name encoded using syntax of encoded-words from MIME.
+// function mimeEncode(name) {
+//   return (
+//     "=?utf-8?b?" +
+//     Buffer.from(name).toString('base64') +
+//     "?="
+//   );
+// }
 
 /**
  * Calls the callback so that it redirects to question form URL.
  *
  * Optional code can be specified. This code is set as a fragment part
  * of the redirect location.
- *
- * @param {!NetlifyCallback} callback
- * @param {string=} code
  */
 function redir(callback, code) {
   callback(null, {
@@ -63,20 +40,9 @@ function redir(callback, code) {
   });
 }
 
-/**
- * Parses and validates event triggered by question form submission.
- *
- * The function ends with a call to {@link sendEmail}.
- *
- * @param {!Object} event
- * @param {!Object} context
- * @param {!NetlifyCallback} callback
- */
+//Parses and validates event triggered by question form submission.
 function sendIdea(event, context, callback) {
-  console.log("hitSendIdea function");
-  //console.log(event["headers"]);
-  console.log(event["body"]);
-  if (event["httpMethod"] !== "POST") {
+   if (event["httpMethod"] !== "POST") {
     return callback(
       new Error(`Unexpected HTTP method "${event["httpMethod"]}"`)
     );
@@ -90,45 +56,29 @@ function sendIdea(event, context, callback) {
 
   const params = JSON.parse(event["body"]);
 
+//    ******leaving this in until honeypot is setup****** 
 //   if (process.env["QUESTION_FORM_HONEYPOT"] &&
 //       params[process.env["QUESTION_FORM_HONEYPOT"]]) {
 //     console.info("Bot trapped in honeypot")
 //     return callback()
 //   }
-  console.log(params);  
+    
   const errs = [];
-  console.log('HIt errors section')
+  
   if (!params["name"]) errs.push("no-name");
   if (!params["email"]) errs.push("no-email");
   if (!params["idea"]) errs.push("no-idea");
-  console.log(errs);
-  //if (errs.length > 0) return redir(callback, errs.join(","));
-  console.log(params['name']);
-  console.log(params.name);
-  
-  sendEmail(
-    params["name"],  
-    params["email"],
-    params["idea"],
-    callback
-  );
-}
+  if (errs.length > 0) return redir(callback, errs.join(","));
 
-/**
- * Sends email via AWS SES API.
- *
- * @param {string} replyTo
- * @param {string} text
- * @param {!NetlifyCallback} callback
- */
+  sendEmail(params["name"], params["email"], params["idea"], callback);}
+
+ //Sends email via AWS SES API.
 function sendEmail(name, replyTo, text, callback) {
-  console.log("Hit Email Function");
-  console.log('Email has env?'+ process.env["IDEA_FORM_FROM"] )
   ses.sendEmail({
     Source: process.env["IDEA_FORM_FROM"],
     Destination: {
       ToAddresses: [
-	process.env["IDEA_FORM_TO"]
+	      process.env["IDEA_FORM_TO"]
       ]
     },
     ReplyToAddresses: [
@@ -136,14 +86,14 @@ function sendEmail(name, replyTo, text, callback) {
     ],
     Message: {
       Subject: {
-	Charset: "UTF-8",
-	Data: process.env["IDEA_FORM_SUBJECT"]
+	      Charset: "UTF-8",
+	      Data: process.env["IDEA_FORM_SUBJECT"]
       },
       Body: {
-	Text: {
-	  Charset: "UTF-8",
-	  Data: text
-	}
+	      Text: {
+	        Charset: "UTF-8",
+	        Data: text
+	      }
       }
     }
   }, (err, data) => {
@@ -151,7 +101,6 @@ function sendEmail(name, replyTo, text, callback) {
       console.error("Error while sending email via AWS SES:", err);
       return redir(callback, "fail");
     }
-
     redir(callback, "sent");
   });
 }
