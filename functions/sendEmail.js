5ac6ca7c81ad4989d5c14b69f9f7b402383d394e
Mike Roberts <mikebroberts@hotmail.com>
2019-10-15 22:20:48 -0600

    cleaning up code a litte and adding basic message formatting

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,28 +1,21 @@
 //jshint esversion: 6
 
 const AWS = require("aws-sdk");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const env = dotenv.config().parsed;
 
 const ses = new AWS.SES({
-  region: process.env["MY_AWS_REGION"],
-  credentials: new AWS.Credentials(process.env["MY_AWS_ACCESS_KEY_ID"],
-  process.env["MY_AWS_SECRET_ACCESS_KEY"])
+  region: process.env["SES_AWS_REGION"],
+  credentials: new AWS.Credentials(
+    process.env["SES_AWS_ACCESS_KEY_ID"],
+    process.env["SES_AWS_SECRET_ACCESS_KEY"]
+  )
 });
 
- //Returns just type and subtype from Content-Type HTTP header value.
+//Returns just type and subtype from Content-Type HTTP header value.
 function parseContentType(headerValue) {
   return (headerValue || "").split(/;\s+/, 2)[0];
 }
-
-// // Returns name encoded using syntax of encoded-words from MIME.
-// function mimeEncode(name) {
-//   return (
-//     "=?utf-8?b?" +
-//     Buffer.from(name).toString('base64') +
-//     "?="
-//   );
-// }
 
 /**
  * Calls the callback so that it redirects to question form URL.
@@ -32,23 +25,25 @@ function parseContentType(headerValue) {
  */
 function redir(callback, code) {
   callback(null, {
-    "statusCode": 200,
-    "headers": {
-      "Location": process.env["IDEA_FORM_URL"]
+    statusCode: 200,
+    headers: {
+      Location: process.env["IDEA_FORM_URL"]
     },
-    "body": code
+    body: code
   });
 }
 
 //Parses and validates event triggered by question form submission.
 function sendIdea(event, context, callback) {
-   if (event["httpMethod"] !== "POST") {
+  if (event["httpMethod"] !== "POST") {
     return callback(
       new Error(`Unexpected HTTP method "${event["httpMethod"]}"`)
     );
   }
-  if (parseContentType(event["headers"]["content-type"]) !==
-      "application/x-www-form-urlencoded") {
+  if (
+    parseContentType(event["headers"]["content-type"]) !==
+    "application/x-www-form-urlencoded"
+  ) {
     return callback(
       new Error(`Unexpected content type "${event["headers"]["content-type"]}"`)
     );
@@ -56,53 +51,57 @@ function sendIdea(event, context, callback) {
 
   const params = JSON.parse(event["body"]);
 
-//    ******leaving this in until honeypot is setup****** 
-//   if (process.env["QUESTION_FORM_HONEYPOT"] &&
-//       params[process.env["QUESTION_FORM_HONEYPOT"]]) {
-//     console.info("Bot trapped in honeypot")
-//     return callback()
-//   }
-    
+  //    ******leaving this in until honeypot is setup******
+  //   if (process.env["QUESTION_FORM_HONEYPOT"] &&
+  //       params[process.env["QUESTION_FORM_HONEYPOT"]]) {
+  //     console.info("Bot trapped in honeypot")
+  //     return callback()
+  //   }
+
   const errs = [];
-  
+
   if (!params["name"]) errs.push("no-name");
   if (!params["email"]) errs.push("no-email");
   if (!params["idea"]) errs.push("no-idea");
   if (errs.length > 0) return redir(callback, errs.join(","));
+  let email = params["email"];
+  let name = params["name"];
+  let idea = params["idea"];
+  let subject = `Here is a new idea from ${name} at ${email}: ${idea}`;
 
-  sendEmail(params["name"], params["email"], params["idea"], callback);}
+  sendEmail(name, email, subject, callback);
+}
 
- //Sends email via AWS SES API.
+//Sends email via AWS SES API.
 function sendEmail(name, replyTo, text, callback) {
-  ses.sendEmail({
-    Source: process.env["IDEA_FORM_FROM"],
-    Destination: {
-      ToAddresses: [
-	      process.env["IDEA_FORM_TO"]
-      ]
-    },
-    ReplyToAddresses: [
-      replyTo
-    ],
-    Message: {
-      Subject: {
-	      Charset: "UTF-8",
-	      Data: process.env["IDEA_FORM_SUBJECT"]
+  ses.sendEmail(
+    {
+      Source: process.env["IDEA_FORM_FROM"],
+      Destination: {
+        ToAddresses: [process.env["IDEA_FORM_TO"]]
       },
-      Body: {
-	      Text: {
-	        Charset: "UTF-8",
-	        Data: text
-	      }
+      ReplyToAddresses: [replyTo],
+      Message: {
+        Subject: {
+          Charset: "UTF-8",
+          Data: process.env["IDEA_FORM_SUBJECT"]
+        },
+        Body: {
+          Text: {
+            Charset: "UTF-8",
+            Data: text
+          }
+        }
       }
+    },
+    (err, data) => {
+      if (err) {
+        console.error("Error while sending email via AWS SES:", err);
+        return redir(callback, "fail");
+      }
+      redir(callback, "sent");
     }
-  }, (err, data) => {
-    if (err) {
-      console.error("Error while sending email via AWS SES:", err);
-      return redir(callback, "fail");
-    }
-    redir(callback, "sent");
-  });
+  );
 }
 
 exports.handler = sendIdea;
