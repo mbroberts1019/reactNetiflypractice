ffef8e46ec191551918faa26dd5f1ba2785319d1
Mike Roberts <mikebroberts@hotmail.com>
2019-10-20 15:29:04 -0600

    adding database storage to form

---
+++ functions/storeIdea.js
@@ -0,0 +1,106 @@
+// Load the AWS SDK for Node.js
+const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+const env = dotenv.config().parsed;
+
+// Set the region and credentials
+AWS.config.update({
+  region: process.env["SES_AWS_REGION"],
+  credentials: new AWS.Credentials(
+    process.env["DDB_AWS_ACCESS_KEY_ID"],
+    process.env["DDB_AWS_SECRET_ACCESS_KEY"]
+  )
+});
+
+//Returns just type and subtype from Content-Type HTTP header value.
+function parseContentType(headerValue) {
+  return (headerValue || "").split(/;\s+/, 2)[0];
+}
+
+// Calls the callback so that it redirects to question form URL.
+
+function redir(callback, code) {
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      Location: process.env["IDEA_FORM_URL"]
+    },
+    body: code
+  });
+}
+
+//Parses and validates event triggered by question form submission.
+function sendToDB(event, context, callback) {
+  if (event["httpMethod"] !== "POST") {
+    return callback(
+      new Error(`Unexpected HTTP method "${event["httpMethod"]}"`)
+    );
+  }
+  if (
+    parseContentType(event["headers"]["content-type"]) !==
+    "application/x-www-form-urlencoded"
+  ) {
+    return callback(
+      new Error(`Unexpected content type "${event["headers"]["content-type"]}"`)
+    );
+  }
+
+  const params = JSON.parse(event["body"]);
+
+  //    ******leaving this in until honeypot is setup******
+  //   if (process.env["QUESTION_FORM_HONEYPOT"] &&
+  //       params[process.env["QUESTION_FORM_HONEYPOT"]]) {
+  //     console.info("Bot trapped in honeypot")
+  //     return callback()
+  //   }
+
+  const errs = [];
+
+  if (!params["name"]) errs.push("no-name");
+  if (!params["email"]) errs.push("no-email");
+  if (!params["idea"]) errs.push("no-idea");
+  if (errs.length > 0) return redir(callback, errs.join(","));
+  let email = params["email"];
+  let name = params["name"];
+  let idea = params["idea"];
+  let generatedId = createNewId();
+
+  putNewItem(name, email, generatedId, idea, callback);
+}
+
+// Create the DynamoDB service object
+function putNewItem(name, email, itemId, idea) {
+  var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+  var params2 = {
+    TableName: "IdeaPracticeTable",
+    Item: {
+      IdeaId: { S: itemId },
+      Contributer_Name: { S: name },
+      Contributor_Email: { S: email },
+      Idea: { S: idea }
+    }
+  };
+
+  // Call DynamoDB to add the item to the table
+  ddb.putItem(params2, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+}
+
+// generate id by appending random 2 digit number with date/time stamp
+function createNewId() {
+  let randNum = Math.floor(Math.random() * 100);
+  return "" + randNum + getTime() + "";
+}
+
+function getTime() {
+  let now = new Date().getTime();
+  return now;
+}
+
+exports.handler = sendToDB;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import NetlifyForm from 'react-netlify-form';
 
 export default function NetiflyFormExample() {
   return (
@@ -13,14 +12,12 @@ export default function NetiflyFormExample() {
           <li>
             <Link to="/form">Form</Link>
           </li>
-          
         </ul>
 
         <hr />
 
         <Route exact path="/" component={Home} />
         <Route path="/form" component={Form} />
-        
       </div>
     </Router>
   );
@@ -37,95 +34,117 @@ function Home() {
 function Form() {
   return (
     <div>
-      <Banner/>
-      <IdeaForm/>
+      <Banner />
+      <IdeaForm />
     </div>
   );
 }
 
-const submitFormStyle = {padding: '20px'};
-const bannerStyle = {color: 'purple'};
+const submitFormStyle = { padding: "20px" };
+const bannerStyle = { color: "purple" };
 
-const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 };
 
- 
-  class Banner extends React.Component{
-
-    render(){
-      return(
-        <div style= {bannerStyle}>I Make Good Forms!!!</div>
-      );
-    }
-
+class Banner extends React.Component {
+  render() {
+    return <div style={bannerStyle}>I Make Good Forms!!!</div>;
   }
- 
-  class IdeaForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {name: '', idea: ''};
-    }
-  
-    // handleSubmit(e) {
-    //     fetch("/", {
-    //       method: "POST",
-    //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //       body: encode({ "form-name": "submission", ...this.state.idea })
-    //     })
-    //       .then(() => alert("Success!"))
-    //       .catch(error => alert(error));
-  
-    //     e.preventDefault();
-    //   }
-    
-    
-    handleChange(event) {
-      this.setState({[event.target.name]: event.target.value});
-    }
-  
-    onSubmit(event) {
-      alert( this.state.name + ' @ '+ this.state.email  +' has an idea: ' + this.state.idea);
-      
-      fetch('https://boring-brattain-746f09.netlify.com/.netlify/functions/sendEmail', {
-        method: 'POST',
+}
+
+class IdeaForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", idea: "" };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    alert(
+      this.state.name +
+        " @ " +
+        this.state.email +
+        " has an idea: " +
+        this.state.idea
+    );
+
+    fetch(
+      "https://boring-brattain-746f09.netlify.com/.netlify/functions/storeIdea",
+      {
+        method: "POST",
         headers: {
-          "Access-Control-Allow-Origin" : "*",
-          'Content-Type': 'application/x-www-form-urlencoded',
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/x-www-form-urlencoded"
         },
         body: JSON.stringify({
           email: `${this.state.email}`,
           name: `${this.state.name}`,
           idea: `${this.state.idea}`
-          })
         })
-      
-      
-        event.preventDefault();
-    }
-  
-    render() {
-      return (
+      }
+    );
 
-        <div style = {submitFormStyle}>
-                <form name= "cool form" onSubmit={(e) => this.onSubmit(e)} >
-                  <label>
-                    Have a great Idea?
-                  </label><br/>
-                  <input type= 'text' value= {this.state.name} name= 'name' placeholder= 'Your Name' onChange= {(e)=>this.handleChange(e)}></input> <br/>
-                  <input type= 'text' value= {this.state.email} name= 'email' placeholder= 'awesomeperson@email.com' onChange= {(e)=>this.handleChange(e)}></input><br/>
-                
-                    <textarea rows= "20" cols = "80" name= 'idea' value= {this.state.idea} id="submittedIdea" type="text" placeholder="Enter your idea here" onChange= {(e)=>this.handleChange(e)}></textarea><br></br>
-                    <button type="submit">Submit</button>
-                </form>
-        </div>
-           
-    
-      );
-    }
+    fetch(
+      "https://boring-brattain-746f09.netlify.com/.netlify/functions/sendEmail",
+      {
+        method: "POST",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: JSON.stringify({
+          email: `${this.state.email}`,
+          name: `${this.state.name}`,
+          idea: `${this.state.idea}`
+        })
+      }
+    );
+
+    event.preventDefault();
   }
 
-
-
+  render() {
+    return (
+      <div style={submitFormStyle}>
+        <form name="cool form" onSubmit={e => this.onSubmit(e)}>
+          <label>Have a great Idea?</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.name}
+            name="name"
+            placeholder="Your Name"
+            onChange={e => this.handleChange(e)}
+          ></input>{" "}
+          <br />
+          <input
+            type="text"
+            value={this.state.email}
+            name="email"
+            placeholder="awesomeperson@email.com"
+            onChange={e => this.handleChange(e)}
+          ></input>
+          <br />
+          <textarea
+            rows="20"
+            cols="80"
+            name="idea"
+            value={this.state.idea}
+            id="submittedIdea"
+            type="text"
+            placeholder="Enter your idea here"
+            onChange={e => this.handleChange(e)}
+          ></textarea>
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
