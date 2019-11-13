d5a5431dd35b06ff06f96a578408f661566ecf15
Mike Roberts <mikebroberts@hotmail.com>
2019-11-12 21:29:45 -0700

    cleaning up recaptcha

--- functions/recaptchaCheck.js
+++ functions/recaptchaCheck.js
@@ -4,48 +4,21 @@ const env = dotenv.config().parsed;
 
 const secretKey = process.env["RECAPTCHA_SECRET_KEY"];
 
+const recaptchaCheck = async (event, context) => {
 
-
-
-const recaptchaCheck = async(event, context) => {
-    
     const params = JSON.parse(event["body"]);
     let token = params["token"];
     let googleRecaptchaApiURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
-    
-    
+    console.log("Token => ", token);
+    console.log("URl => ", googleRecaptchaApiURL);
     return fetch(googleRecaptchaApiURL, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};
-    
-    
-    
-    
-    
-    callback(null, {
-        statusCode: 200,
-        body: `This only took me ${Math.floor(Math.random() * 10)} hours`
-        
-    });
+        .then(response => response.json())
+        .then(data => ({
+            statusCode: 200,
+            body: data
+        }))
+        .catch(error => ({ statusCode: 422, body: String(error) }));
 };
 
 exports.handler = recaptchaCheck;
 
-import fetch from "node-fetch";
-
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
-
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data.joke
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};
